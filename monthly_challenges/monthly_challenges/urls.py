a8eff25c1ddf59b05c69ab7dd4de5283f678c579
Vitor-Guimaraes <guimvitor@gmail.com>
2024-05-22 20:26:57 -0300

    Creating monthly_challenges project

--- monthly_challenges/monthly_challenges/urls.py
+++ monthly_challenges/monthly_challenges/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mypage project.
+URL configuration for monthly_challenges project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
