b294a146a33612f393925ba9119c1b765cdb74de
Vitor-Guimaraes <guimvitor@gmail.com>
2024-05-24 17:07:56 -0300

    Added DTL to challenges page

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -33,7 +33,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month.capitalize(),
+        })
     except:
         return HttpResponseNotFound("<h1>Month not supported</h1>")
     
