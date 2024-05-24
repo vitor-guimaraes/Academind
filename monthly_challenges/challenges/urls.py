aaeb1b29aeb2fa3b883f70f2baec8124cb9d0d3c
Vitor-Guimaraes <guimvitor@gmail.com>
2024-05-24 14:19:25 -0300

    Redirecting URLS

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
+    path("", views.index),
 ]
