f32e2db2d5ce5ca31f91fd54ecffb3ac4a5b7dab
Vitor-Guimaraes <guimvitor@gmail.com>
2024-05-25 19:08:23 -0300

    Created crude starting page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+
+# 1. Create View
+# 2. Create URL paths, linking views.py with urls.py
+# 3. Link App URL with Project URL
+# 4. Create HTML page on templates folder
+# 5. Add app pto installed apps on settings.py
+
+
+def index(request):
+    # try:
+    #     return render(request, "blog/index.html")
+    # except:
+    #     raise Http404()
+    return render(request, "blog/index.html")
+
+
+
+
