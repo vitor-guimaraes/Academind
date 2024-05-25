b7c218fe883d8f79b18da7045d2a5d91db1c1797
Vitor-Guimaraes <guimvitor@gmail.com>
2024-05-25 19:45:49 -0300

    Created details page

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("posts", views.posts, name="posts")
+    path("posts", views.posts, name="posts"),
+    path("posts/<slug:slug>", views.detail_post, name="details")
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, Http404
 # 1. Create View
 # 2. Create URL paths, linking views.py with urls.py
 # 3. Create HTML page on templates folder
-# 4. Reference App.urls file on Project.urls file
+# 4. Include App.urls file on Project.urls file
 # 5. Add app to installed apps on Project settings.py
 
 
@@ -22,3 +22,9 @@ def posts(request):
     except:
         raise Http404()
 
+
+def detail_post(request, slug):
+    try:
+        return render(request, "blog/details.html")
+    except:
+        raise Http404()
