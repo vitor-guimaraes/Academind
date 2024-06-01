b28fab841b9647872c7d760ce20417693da5a3be
Vitor-Guimaraes <guimvitor@gmail.com>
2024-06-01 11:23:19 -0300

    Updated details url to use slug

--- bookstore/bookoutlet/views.py
+++ bookstore/bookoutlet/views.py
@@ -11,11 +11,11 @@ def index(request):
     })
 
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # try:
     #     book = Book.objects.get(pk=id)
     # except: raise Http404()
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
 
     return render(request, "bookoutlet/book_detail.html", {
         "title": book.title,
