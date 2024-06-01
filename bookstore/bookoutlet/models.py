bd57cf128da7f722ce0e123a157ec849baf14db9
Vitor-Guimaraes <guimvitor@gmail.com>
2024-06-01 10:56:42 -0300

    Updated index.html to link with details page

--- bookstore/bookoutlet/models.py
+++ bookstore/bookoutlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
@@ -10,3 +11,7 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
+    
