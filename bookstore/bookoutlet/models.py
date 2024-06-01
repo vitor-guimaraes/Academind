40e1d950506dcc47e148ad948f8e38337fee9f77
Vitor-Guimaraes <guimvitor@gmail.com>
2024-06-01 11:17:13 -0300

    Updated book model to contain slug field

--- bookstore/bookoutlet/models.py
+++ bookstore/bookoutlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
@@ -8,6 +9,7 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(6)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
@@ -15,3 +17,6 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])
     
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
