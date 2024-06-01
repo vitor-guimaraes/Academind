4b25a0f7cb8c6818c2db25674713f413f519ff52
Vitor-Guimaraes <guimvitor@gmail.com>
2024-06-01 10:46:55 -0300

    Added data do teails page

--- bookstore/bookoutlet/views.py
+++ bookstore/bookoutlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from . models import Book
 
@@ -7,4 +8,18 @@ def index(request):
     books = Book.objects.all()
     return render(request, "bookoutlet/index.html", {
         "books": books
+    })
+
+
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except: raise Http404()
+    book = get_object_or_404(Book, pk=id)
+
+    return render(request, "bookoutlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling
     })
