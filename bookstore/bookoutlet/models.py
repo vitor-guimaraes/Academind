e93383810455d05f49102c48f36229208a7b98db
Vitor-Guimaraes <guimvitor@gmail.com>
2024-06-03 19:47:51 -0300

    Updated models (added Country)

--- bookstore/bookoutlet/models.py
+++ bookstore/bookoutlet/models.py
@@ -4,10 +4,27 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+    
+    class Meta:
+        verbose_name_plural = "Address Entries"
 
 
 class Author(models.Model):
@@ -29,6 +46,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
